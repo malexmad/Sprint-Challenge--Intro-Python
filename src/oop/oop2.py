2d745cc42109570fd2247b7aceb9fdfb2a7100e7
malex-01 <malex140@gmail.com>
2020-03-13 13:46:20 -0400

    typo oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels = 4):
+    def __init__(self, num_wheels=4):
 
         self.num_wheels = num_wheels
 
@@ -20,8 +20,8 @@ class GroundVehicle():
 
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
 
-        self.num_wheels = num_wheels
 
     def drive(self):
         return "BRAAAP!!"
